f08c2b3bd702519937b0de962f662aae6e3302eb
IMAM SODIK <imamsodik00@gmail.com>
2026-04-21 15:35:17 +0700

    adf

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\OrderController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\MidtransController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/midtrans/callback', [OrderController::class, 'callback']);
+Route::post('/midtrans/callback', [MidtransController::class, 'callback']);
