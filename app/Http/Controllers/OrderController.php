1116042f3f6f33e96f7e25f038e82a01327c3861
IMAM SODIK <imamsodik00@gmail.com>
2026-04-21 15:31:43 +0700

    adf

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,12 +9,20 @@ use Midtrans\Snap;
 use App\Models\Menu;
 use App\Models\Order;
 use App\Models\OrderItem;
+use Midtrans\Config;
 
 class OrderController extends Controller
 {
+    public function __construct()
+    {
+        Config::$serverKey = config('midtrans.server_key');
+        Config::$isProduction = config('midtrans.is_production');
+        Config::$isSanitized = config('midtrans.is_sanitized');
+        Config::$is3ds = config('midtrans.is_3ds');
+    }
+
     public function checkout(Request $request)
     {
-        dd(config('midtrans.server_key'));
         $request->validate([
             'items' => 'required|array|min:1',
             'items.*.id' => 'required',
